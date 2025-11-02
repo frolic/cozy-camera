1f466676ae73d005a417095f17d612e1158372a3
Kevin Ingersoll <kingersoll@gmail.com>
2025-11-02 13:14:16 +0000

    try prod build

--- src/root.tsx
+++ src/root.tsx
@@ -56,7 +56,7 @@ createRoot(document.getElementById("root")!).render(
           </Routes>
         </Layout>
       </BrowserRouter>
-      <JazzInspector />
+      {import.meta.env.DEV ? <JazzInspector /> : null}
     </JazzReactProvider>
   </StrictMode>
 );
